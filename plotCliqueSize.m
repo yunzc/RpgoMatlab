3f824401c7af84ea2e5706f470122c572d46e928
yunchang <yunchang@mit.edu>
2019-07-08 17:45:36 -0400

    updated clique plotting code

--- plotCliqueSize.m
+++ plotCliqueSize.m
@@ -17,14 +17,17 @@ function plotCliqueSize(dataFile)
         end
     end
     
-    widths = data(2:length(data),1) - data(1:length(data)-1,1);
-    widths = nonzeros(widths);
-    binwidth = min(widths);
-        
+    edges = (data(1:length(data)-1,1) + data(2:length(data),1))/2.0;
+    edges = [data(1,1) - (edges(1,1) - data(1,1)); edges];
+    edges = [edges; data(length(data),1) + (data(length(data),1) - edges(length(edges),1))];
     
     figure();
-    h = histogram(histdata, 'BinWidth', binwidth);
+    h = histogram(histdata, edges);
     xlabel("threshold")
     ylabel("clique size")
+    hold on
+    height = max(h.Values);
+    plot(data(:,1), height * chi2cdf(data(:,1),1), 'r','LineWidth',3)
+    legend('# iniers','chi squared cdf','Location','northwest')
 end
 
