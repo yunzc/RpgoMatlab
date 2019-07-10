99d06e528d7d97297f2100aeb0ad0cc79b70cdab
yunchang <yunchang@mit.edu>
2019-07-10 11:57:30 -0400

    small edits to clq size plot

--- plotCliqueSize.m
+++ plotCliqueSize.m
@@ -9,25 +9,29 @@ function plotCliqueSize(dataFile)
         row = str2num(line);
         data = [data; row];
     end
-    data = sortrows(data, 1);
+    data = 1.7 * sortrows(data, 1); % scale 2.5-3 works for 3d
     histdata = [];
     for i = 1:length(data)
         for j = 1:data(i,2)
             histdata = [histdata, data(i,1)];
         end
     end
-    
+   
     edges = (data(1:length(data)-1,1) + data(2:length(data),1))/2.0;
     edges = [data(1,1) - (edges(1,1) - data(1,1)); edges];
     edges = [edges; data(length(data),1) + (data(length(data),1) - edges(length(edges),1))];
+%     binwidth = min(data(2:length(data),1) - data(1:length(data)-1,1));
     
     figure();
     h = histogram(histdata, edges);
+%     h = histogram(histdata, 'BinWidth', binwidth);
     xlabel("threshold")
     ylabel("clique size")
     hold on
     height = max(h.Values);
-    plot(data(:,1), height * chi2cdf(data(:,1),1), 'r','LineWidth',3)
-    legend('# iniers','chi squared cdf','Location','northwest')
+%     plot(data(:,1), height * chi2cdf(data(:,1),4), 'r','LineWidth',3)
+%     plot(data(:,1), height * chi2cdf(data(:,1),5), 'b','LineWidth',3)
+    plot(data(:,1), height * chi2cdf(data(:,1),6), 'g','LineWidth',3)
+    legend('# iniers','chi2 cdf 6', 'Location','northwest')
 end
 
